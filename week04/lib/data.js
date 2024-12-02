fbaad916bcc674e6fe9395609707d89eda3e12b1
Vernice Jaramillo <jaramillov92@gmail.com>
2024-12-02 11:57:37 -0800

    december

--- week04/lib/data.js
+++ week04/lib/data.js
@@ -1,4 +1,3 @@
-// i need the node default modules for fs and path
 // import fs from 'fs';
 // import path from 'path';
 
@@ -9,7 +8,7 @@ import got from 'got';
 
 //url endpoint
 const dataURL = 
-"https://dev-vs-fall-24-site-2.pantheonsite.io/wp-json/twentytwentyone-child/v1/latest-posts/1"
+"https://dev-vs-fall-24-site-2.pantheonsite.io/wp-json/twentytwentyone-child/v1/special"
 
 // function returns names and ids for all json objects in array, sorted by name property
 export async function getSortedList() {
@@ -33,16 +32,35 @@ export async function getSortedList() {
   //const jsonObj = JSON.parse(jsonString);
 const jsonObj = JSON.parse(jsonString.body);
 
+// here is where you would run the forEach to convert acf_fields string data into JSON
+  // loop thru each object in array returned as JSON
+  jsonObj.forEach(
+    function(item) {
+      // reformat string contained in delimited acf field data, add curlies and quotes
+      let x = '{"' + item.acf_fields + '"}';
+      // https://www.w3schools.com/jsref/jsref_replace.asp
+      // x = x.replace(/,/g,'","');
+      x = x.replaceAll(',','","');
+      // x = x.replace(/:/g,'":"');
+      x = x.replaceAll(':','":"');
+      // now that we have a string that is in valid json format, convert it to json
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
+      let y = JSON.parse(x);
+      console.log(y);
+      console.log(y.first_name);
+      item.acf_fields = y;
+    }
+  );
+
   // sort json array by name property
   jsonObj.sort(
     function(a,b) {
-      return a.post_title.localeCompare(b.name);
+      return a.post_title.localeCompare(b.post_title);
     }
   );
 
   // use map() on array to extract just id + name properties into new array of obj values
-  return jsonObj.map(
-    function(item) {
+  return jsonObj.map(item => {
       return {
         id: item.ID.toString(),
         name: item.post_title
@@ -61,7 +79,7 @@ export  async function getAllIds() {
   let jsonString;
   try {
     jsonString = await got(dataURL);
-    console.log(jsonString.body);
+    //console.log(jsonString.body);
 
   } catch(error) {
     jsonString.body = [];
@@ -73,13 +91,12 @@ export  async function getAllIds() {
   const jsonObj = JSON.parse(jsonString.body);
 
   // use map() on array to extract just id + name properties into new array of obj values
-  return jsonObj.map(
-    function(item) {
+  return jsonObj.map(item => {
       return {
         params: {
           id: item.ID.toString()
         }
-      };
+      }
     }
   );
   
@@ -107,21 +124,21 @@ export async function getData(idRequested) {
   const jsonObj = JSON.parse(jsonString.body);
 
   // find object value in array that has matching id
-  const objMatch = jsonObj.filter(
-    function(obj) {
+  const objMatch = jsonObj.filter(obj => {
       return obj.ID.toString() === idRequested;
-    }
-  );
+    });
+
 
   // extract object value in filtered array if any
   let objReturned;
   if (objMatch.length > 0) {
     objReturned = objMatch[0];
 
-    
+
   } else {
     objReturned = {};
   }
+  //console.log(objReturned);
 
    //return object value found
   return objReturned;
--- week04/pages/[id].js
+++ week04/pages/[id].js
@@ -1,8 +1,8 @@
+import Head from 'next/head';
 import Layout from '../components/layout';
 import { getAllIds, getData } from '../lib/data';
 
-// define a getStaticProps() function to have next.js retrieve data to use for the dynamic page
-// - this name is defined by next.js
+
 export async function getStaticProps( { params } ) {
   const itemData = await getData(params.id);
   return {
@@ -12,8 +12,7 @@ export async function getStaticProps( { params } ) {
   };
 }
 
-// define a getStaticPaths() function to tell next.js all valid URLs: 1,2,3,4 
-// - this name is defined by next.js
+
 export async function getStaticPaths() {
   const paths = await getAllIds();
   return {
@@ -22,16 +21,16 @@ export async function getStaticPaths() {
   };
 }
 
-// export our dynamically routed page component Entry
+
 export default function Entry( { itemData } ) {
   return (
     <Layout>
       <article className="card col-6">
         <div className="card-body">
-          <h4 className="card-title">{itemData.name}</h4>
-          <h5 className="card-subtitle mb-2 text-body-secondary">{itemData.phone}</h5>
-          <p className="card-text">{itemData.birthdate}</p>
-          <a href="#" className="card-link">{itemData.email}</a>
+          <h4 className="card-title">{itemData.post_title}</h4>
+          <h5 className="card-subtitle mb-2 text-muted">{itemData.user_login}</h5>
+          <div className="card-text" dangerouslySetInnerHTML={{__html: itemData.post_content}}/>
+          
           <h6>Characteristics</h6>
           <ol>
             {itemData.characteristic && itemData.characteristic.map(
